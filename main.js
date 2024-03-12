e6ea26577afaaf0aaad3923c79ffb59606477fdb
Neirpy <48681341+Neirpy@users.noreply.github.com>
2024-03-13 00:13:48 +0100

    add: yotj

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let slideIndex = 0;
 const slides = document.querySelectorAll('.slide');
+const slidesBook = document.querySelectorAll('.slideBook');
 const slideCounter = document.getElementById('slideCounter');
 let slideInterval;
 
@@ -86,13 +87,89 @@ function prevSlide() {
     showSlide();
 }
 
-if (document.querySelector('.next') && document.querySelector('.prev')){
+if (document.querySelector('.slider')){
     document.querySelector('.next').addEventListener('click', nextSlide);
     document.querySelector('.prev').addEventListener('click', prevSlide);
     generateSlideCounter();
     showSlide();
 }
 
+
+let slideBookIndex = 0;
+const slideBookCounter = document.getElementById('slideBookCounter');
+let slideBookInterval;
+
+function generateSlideBookCounter() {
+    const selectList = document.createElement('select');
+    selectList.classList.add('slideBookSelectList');
+    for (let i = 0; i < slidesBook.length; i++) {
+        const option = document.createElement('option');
+        option.classList.add('slideBookOption');
+        option.value = i;
+        option.text = i + 1;
+        selectList.appendChild(option);
+    }
+    selectList.addEventListener('change', () => {
+        slideBookIndex = selectList.value;
+        clearInterval(slideBookInterval);
+        showSlideBook();
+        slideBookInterval = setInterval(nextSlideBook, 3000);
+    });
+
+    const totalSlidesBook = document.createElement('span');
+    totalSlidesBook.textContent = ` / ${slidesBook.length}`;
+
+    slideBookCounter.appendChild(selectList);
+    slideBookCounter.appendChild(totalSlidesBook);
+}
+
+function hideAllSlideBooks() {
+    for (let slide of slidesBook) {
+        slide.classList.remove('activeSlideBook');
+        slide.classList.remove('slideBook-left');
+        slide.style.display = 'none';
+    }
+}
+
+function showSlideBook() {
+    hideAllSlideBooks();
+    slidesBook[slideBookIndex].classList.add('activeSlideBook');
+    slidesBook[slideBookIndex].style.display = 'flex';
+
+    const selectList = document.querySelector('.slideBookSelectList');
+    selectList.value = slideBookIndex;
+    updateSlideBookCounter();
+}
+
+function updateSlideBookCounter() {
+    const buttons = slideBookCounter.querySelectorAll('button');
+    buttons.forEach((button, index) => {
+        if (index === slideBookIndex) {
+            button.classList.add('activeBtn');
+        } else {
+            button.classList.remove('activeBtn');
+        }
+    });
+}
+
+function nextSlideBook() {
+    slideBookIndex++;
+    if (slideBookIndex >= slidesBook.length) slideBookIndex = 0;
+    showSlideBook();
+}
+
+function prevSlideBook() {
+    slideBookIndex--;
+    if (slideBookIndex < 0) slideBookIndex = slidesBook.length - 1;
+    showSlideBook();
+}
+
+if (document.querySelector('.sliderBook')){
+    document.querySelector('.nextBook').addEventListener('click', nextSlideBook);
+    document.querySelector('.prevBook').addEventListener('click', prevSlideBook);
+    generateSlideBookCounter();
+    showSlideBook();
+}
 
 
 /**
